1bffe384cd57999cbd48fe7518e67dff7296580d
unknown <sabbirshuvo006@gmail.com>
2023-03-25 02:27:45 +0600

    Redesing

--- src/Components/Pages/Banner/Banner.js
+++ src/Components/Pages/Banner/Banner.js
@@ -10,8 +10,8 @@ const Banner = () => {
                     <p className='text-start mt-5'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Exercitationem, mollitia. Lorem ipsum dolor sit amet consectetur adipisicing elit. Earum, nesciunt!</p>
 
                     <button className='btn rounded-pill btn-outline-info'>Learn More <span>
-                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-arrow-return-right" viewBox="0 0 16 16">
-                            <path fill-rule="evenodd" d="M1.5 1.5A.5.5 0 0 0 1 2v4.8a2.5 2.5 0 0 0 2.5 2.5h9.793l-3.347 3.346a.5.5 0 0 0 .708.708l4.2-4.2a.5.5 0 0 0 0-.708l-4-4a.5.5 0 0 0-.708.708L13.293 8.3H3.5A1.5 1.5 0 0 1 2 6.8V2a.5.5 0 0 0-.5-.5z" />
+                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-arrow-return-right" viewBox="0 0 16 16">
+                            <path fillRule="evenodd" d="M1.5 1.5A.5.5 0 0 0 1 2v4.8a2.5 2.5 0 0 0 2.5 2.5h9.793l-3.347 3.346a.5.5 0 0 0 .708.708l4.2-4.2a.5.5 0 0 0 0-.708l-4-4a.5.5 0 0 0-.708.708L13.293 8.3H3.5A1.5 1.5 0 0 1 2 6.8V2a.5.5 0 0 0-.5-.5z" />
                         </svg>
                     </span></button>
                 </div>
